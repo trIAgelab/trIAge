8117fc718c6c966e66ba5c04c6bbfcbb18a9e663
Christian Staudt <mail@clstaudt.me>
2023-05-26 16:57:09 +0200

    eyeingbot: reacts to mention

--- triage/eyeingbot.py
+++ triage/eyeingbot.py
@@ -1,10 +1,13 @@
 """ This is a test bot for GitHub based on the octomachinery library."""
+import re
 
 from octomachinery.app.server.runner import run as run_app
 from octomachinery.routing import process_event_actions
 from octomachinery.routing.decorators import process_webhook_payload
 from octomachinery.runtime.context import RUNTIME_CONTEXT
 
+# Define the bot's name
+BOT_NAME = "eyeingbot"
 
 @process_event_actions("issue_comment", {"created"})
 @process_webhook_payload
@@ -21,16 +24,18 @@ async def on_comment(
     organization=None,
 ):
     github_api = RUNTIME_CONTEXT.app_installation_client
-    comment_reactions_api_url = f"{comment['url']}/reactions"
-    await github_api.post(
-        comment_reactions_api_url,
-        preview_api_version="squirrel-girl",
-        data={"content": "eyes"},
+   # Check if the bot was mentioned in the comment using a regex
+    if re.search(r'\@{}\b'.format(BOT_NAME), comment['body']):
+        comment_reactions_api_url = f"{comment['url']}/reactions"
+        await github_api.post(
+            comment_reactions_api_url,
+            preview_api_version="squirrel-girl",
+            data={"content": "eyes"},
     )
 
 
 run_app(
-    name='eyeingbot',
+    name=BOT_NAME,
     version='0.0.1',
     url='https://github.com/apps/eyeingbot',
 )
